bf723e34ee40e4502262e178eb917595cf8401fe
Blue Roses <jevargasv1990@gmail.com>
2018-09-07 15:14:10 -0400

    Made routes and made simple css

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tags
   get '/:username', to: 'profile#index'
+  get '/:username', to: 'gallery#index'
+  get '/:username', to: 'faq#index'
   root 'page#home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_06_171917) do
+ActiveRecord::Schema.define(version: 2018_09_07_173531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2018_09_06_171917) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "faqs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "galleries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "tag", null: false
     t.datetime "created_at", null: false
@@ -53,11 +63,6 @@ ActiveRecord::Schema.define(version: 2018_09_06_171917) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "welcomes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
