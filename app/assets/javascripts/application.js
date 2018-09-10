0da1a45b0d6835158a8e87b8389bdc865d7656e3
Blue Roses <jevargasv1990@gmail.com>
2018-09-09 22:37:31 -0400

    Fixed sidebar and carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,27 +15,3 @@
 //= require turbolinks
 //= require_tree .
 //= require jquery
-
-(function($) {
-    "use strict"; // Start of use strict
-  
-    // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-  
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top)
-      }, 1000, 'easeInOutExpo');
-      event.preventDefault();
-    });
-  
-  })(jQuery); // End of use strict
